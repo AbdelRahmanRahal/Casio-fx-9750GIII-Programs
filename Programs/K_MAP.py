ba7eceedffedf9fe04c22a268a190fca6b49507b
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2024-05-30 00:37:47 +0300

    Removed comments (unfortunately)
    They take about 40% of the file size, and that space is needed on the calculator.

--- Programs/K_MAP.py
+++ Programs/K_MAP.py
@@ -1,4 +1,4 @@
-def mul(x,y): # Multiply 2 minterms
+def mul(x,y):
 	res = []
 	for i in x:
 		if i+"`" in y or (len(i)==2 and i[0] in y):
@@ -10,7 +10,7 @@ def mul(x,y): # Multiply 2 minterms
 			res.append(i)
 	return res
 
-def multiply(x,y): # Multiply 2 expressions
+def multiply(x,y):
 	res = []
 	for i in x:
 		for j in y:
@@ -18,21 +18,21 @@ def multiply(x,y): # Multiply 2 expressions
 			res.append(tmp) if len(tmp) != 0 else None
 	return res
 
-def refine(my_list,dc_list): # Removes don't care terms from a given list and returns refined list
+def refine(my_list,dc_list):
 	res = []
 	for i in my_list:
 		if int(i) not in dc_list:
 			res.append(i)
 	return res
 
-def findEPI(x): # Function to find essential prime implicants from prime implicants chart
+def findEPI(x):
 	res = []
 	for i in x:
 		if len(x[i]) == 1:
 			res.append(x[i][0]) if x[i][0] not in res else None
 	return res
 
-def findVariables(x): # Function to find variables in a meanterm.
+def findVariables(x):
 	var_list = []
 	for i in range(len(x)):
 		if x[i] == '0':
@@ -41,18 +41,18 @@ def findVariables(x): # Function to find variables in a meanterm.
 			var_list.append(chr(i+65))
 	return var_list
 
-def padWithZeros(input_string, desired_length): # Pads a string with zeros
+def padWithZeros(input_string, desired_length):
 	while len(input_string) < desired_length:
 		input_string = '0' + input_string
 	return input_string
 
-def flatten(x): # Flattens a list
+def flatten(x):
 	flattened_items = []
 	for i in x:
 		flattened_items.extend(x[i])
 	return flattened_items
 
-def findMinterms(a): # Function for finding out which minterms are merged.
+def findMinterms(a):
 	gaps = a.count('-')
 	if gaps == 0:
 		return [str(int(a,2))]
@@ -72,7 +72,7 @@ def findMinterms(a): # Function for finding out which minterms are merged.
 		x.pop(0)
 	return temp
 
-def compare(a,b): # Function for checking if 2 minterms differ by 1 bit only
+def compare(a,b):
 	c = 0
 	for i in range(len(a)):
 		if a[i] != b[i]:
@@ -82,7 +82,7 @@ def compare(a,b): # Function for checking if 2 minterms differ by 1 bit only
 				return (False,None)
 	return (True,mismatch_index)
 
-def removeTerms(_chart,terms): # Removes minterms which are already covered from chart
+def removeTerms(_chart,terms):
 	for i in terms:
 		for j in findMinterms(i):
 			try:
@@ -99,12 +99,10 @@ print("Enter don't cares (if any)")
 print("seperated by commas: ")
 dc = input()
 
-# Check if there are any don't care terms entered
 if dc:
-	dc = dc.strip().split(',')
-	dc = [int(i) for i in dc]
+	dc = [int(i) for i in dc.strip().split(',')]
 else:
-	dc = []  # No don't care terms entered
+	dc = []
 
 mt.sort()
 minterms = mt+dc
@@ -112,60 +110,56 @@ minterms.sort()
 size = len(bin(minterms[-1]))-2
 groups,all_pi = {},set()
 
-# Primary grouping
 for minterm in minterms:
 	try:
 		groups[bin(minterm).count('1')].append(padWithZeros(bin(minterm)[2:], size))
 	except KeyError:
 		groups[bin(minterm).count('1')] = [padWithZeros(bin(minterm)[2:], size)]
 
-# Process for creating tables and finding prime implicants
 while True:
 	tmp = groups.copy()
 	groups,m,marked,should_stop = {},0,set(),True
 	l = sorted(list(tmp.keys()))
 	for i in range(len(l)-1):
-		for j in tmp[l[i]]: # Loop which iterates through current group elements
-			for k in tmp[l[i+1]]: # Loop which iterates through next group elements
-				res = compare(j,k) # Compare the minterms
-				if res[0]: # If the minterms differ by 1 bit only
+		for j in tmp[l[i]]:
+			for k in tmp[l[i+1]]:
+				res = compare(j,k)
+				if res[0]:
 					try:
 						groups[m].append(j[:res[1]]+'-'+j[res[1]+1:]) if j[:res[1]]+'-'+j[res[1]+1:] not in groups[m] else None # type: ignore
 					except KeyError:
 						groups[m] = [j[:res[1]]+'-'+j[res[1]+1:]] # type: ignore
 					should_stop = False
-					marked.add(j) # Mark element j
-					marked.add(k) # Mark element k
+					marked.add(j)
+					marked.add(k)
 		m += 1
-	local_unmarked = set(flatten(tmp)).difference(marked) # Unmarked elements of each table
-	all_pi = all_pi.union(local_unmarked) # Adding Prime Implicants to global list
-	if should_stop: # If the minterms cannot be combined further
+	local_unmarked = set(flatten(tmp)).difference(marked)
+	all_pi = all_pi.union(local_unmarked)
+	if should_stop:
 		break
 
-
-# Printing and processing of Prime Implicant chart
-sz = len(str(mt[-1])) # The number of digits of the largest minterm
+sz = len(str(mt[-1]))
 chart = {}
 for i in all_pi:
 	merged_minterms,y = findMinterms(i),0
 	for j in refine(merged_minterms,dc):
-		x = mt.index(int(j))*(sz+1) # The position where we should put 'X'
+		x = mt.index(int(j))*(sz+1)
 		y = x+sz
 		try:
-			chart[j].append(i) if i not in chart[j] else None # Add minterm in chart
+			chart[j].append(i) if i not in chart[j] else None
 		except KeyError:
 			chart[j] = [i]
 
-EPI = findEPI(chart) # Finding essential prime implicants
-removeTerms(chart,EPI) # Remove EPI related columns from chart
+EPI = findEPI(chart)
+removeTerms(chart,EPI)
 
-if(len(chart) == 0): # If no minterms remain after removing EPI related columns
-	final_result = [findVariables(i) for i in EPI] # Final result with only EPIs
-else: # Else follow Petrick's method for further simplification
+if(len(chart) == 0):
+	final_result = [findVariables(i) for i in EPI]
+else:
 	P = [[findVariables(j) for j in chart[i]] for i in chart]
-	while len(P)>1: # Keep multiplying until we get the SOP form of P
+	while len(P)>1:
 		P[1] = multiply(P[0],P[1])
 		P.pop(0)
-	final_result = [min(P[0],key=len)] # Choosing the term with minimum variables from P
-	final_result.extend(findVariables(i) for i in EPI) # Adding the EPIs to final solution
+	final_result = [min(P[0],key=len)]
+	final_result.extend(findVariables(i) for i in EPI)
 print('\nSolution: F = '+' + '.join(''.join(i) for i in final_result))
